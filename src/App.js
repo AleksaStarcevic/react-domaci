2bc2cb08fd25d8254a785ba8a29caf84feb75aa8
AleksaStarcevic <aleksastarcevic1@gmail.com>
2022-02-04 22:05:05 +0100

    Uradjen welcome deo

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Home from "./components/pages/Home";
 function App() {
 	return (
 		<>
 			<Router>
 				<Navbar />
 				<Routes>
-					<Route path="/" exact />
+					<Route path="/" exact element={<Home />} />
 					{/* <Route path='/services' component={Services} />
          <Route path='/products' component={Products} />
          <Route path='/sign-up' component={SignUp} /> */}
